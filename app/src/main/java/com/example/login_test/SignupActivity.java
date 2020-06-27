678eb352e2ffb99fffd78a40803dc6a9d7a4ca83
samuellim18 <samuellim18@yahoo.com>
2020-06-27 14:24:33 +1000

    Fixed errors

--- app/src/main/java/com/example/login_test/SignupActivity.java
+++ app/src/main/java/com/example/login_test/SignupActivity.java
@@ -1,6 +1,15 @@
 package com.example.login_test;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reg_page);
+    }
 }
